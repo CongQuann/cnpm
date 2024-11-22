ad05c49bdd01c2c6b5d850ba0cf84431705dc8d6
trnhau1710 <hauvtct@gmail.com>
2024-11-22 23:14:04 +0700

    Giao diện Report

--- QuanLyHocSinh/__init__.py
+++ QuanLyHocSinh/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,3 +7,4 @@ app.secret_key = 'abcd'  # Đặt chuỗi bí mật tại đây
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/studentmng?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app=app)
+
--- QuanLyHocSinh/index.py
+++ QuanLyHocSinh/index.py
@@ -1,12 +1,21 @@
-from flask import render_template
-
+from flask import render_template, request
+import os
 from QuanLyHocSinh import app
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    from QuanLyHocSinh.admin import *
 
+@app.route("/Administrator", methods=["GET", "POST"])
+def report():
+    # Dữ liệu mẫu bạn muốn hiển thị trong bảng
+    data = [
+        {"class": "10A1", "avg_score": 8.5, "max_score": 10, "min_score": 7, "total_students": 45},
+        {"class": "10A2", "avg_score": 7.8, "max_score": 9, "min_score": 6, "total_students": 50},
+    ]
+    return render_template('Administrator/Report.html', data=data)
+
+if __name__ == '__main__':
     app.run(debug=True)
