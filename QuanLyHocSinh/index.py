053ef7979361f137810440790550aae48c5e9651
trnhau1710 <hauvtct@gmail.com>
2024-11-23 13:55:10 +0700

    update report

--- QuanLyHocSinh/index.py
+++ QuanLyHocSinh/index.py
@@ -1,5 +1,5 @@
-from flask import render_template, request
-import os
+from flask import render_template
+
 from QuanLyHocSinh import app
 
 
@@ -12,10 +12,23 @@ def home():
 def report():
     # Dữ liệu mẫu bạn muốn hiển thị trong bảng
     data = [
-        {"class": "10A1", "avg_score": 8.5, "max_score": 10, "min_score": 7, "total_students": 45},
-        {"class": "10A2", "avg_score": 7.8, "max_score": 9, "min_score": 6, "total_students": 50},
+        {"class": "10A1", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A2", "total_students": 50, "pass": 35, "proportion": 70},
+        {"class": "10A3", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A4", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A5", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A6", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A7", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A8", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A9", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A10", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A11", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A12", "total_students": 45, "pass": 40, "proportion": 55},
+        {"class": "10A13", "total_students": 45, "pass": 40, "proportion": 55},
+
     ]
     return render_template('Administrator/Report.html', data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
