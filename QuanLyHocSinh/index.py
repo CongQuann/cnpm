d3a250f449d98f61426d840bfb59153630fcc801
trnhau1710 <hauvtct@gmail.com>
2024-11-29 22:35:00 +0700

    update teacher mng

--- QuanLyHocSinh/index.py
+++ QuanLyHocSinh/index.py
@@ -246,23 +246,11 @@ def update_subject():
 @app.route("/Administrator/TeacherManagement",methods=["GET","POST"])
 def teacher_mng():
     # Dữ liệu mẫu (sau này thay bằng database)
-    teachers = [
-        {"id": 1, "name": "Nguyễn Văn A", "subject": "Toán"},
-        {"id": 2, "name": "Trần Thị B", "subject": "Vật lý"}
-    ]
+    subjects = Subject.query.all()
 
-    subjects = ["Toán", "Vật lý", "Hóa học", "Sinh học"]
 
-    if request.method == "POST":
-        # Xử lý thêm giáo viên
-        new_teacher = {
-            "id": len(teachers) + 1,  # Tăng ID tự động
-            "name": request.form["teacher_name"],
-            "subject": request.form["subject"]
-        }
-        teachers.append(new_teacher)
 
-    return render_template('Administrator/TeacherManagement.html', teachers=teachers, subjects=subjects)
+    return render_template('Administrator/TeacherManagement.html', subjects=subjects)
 
 
 
